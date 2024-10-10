a01c1a1ae7bdc3dc0a0478fde379d1b224e76054
YouMingYeh <b10705052@ntu.edu.tw>
2024-10-10 15:24:32 +0800

    Refactor SplashContent component to dynamically display random content

--- apps/app/src/modules/splash/components/SplashContent.tsx
+++ apps/app/src/modules/splash/components/SplashContent.tsx
@@ -1,7 +1,21 @@
 export function SplashContent() {
+  const randomContent = contents[Math.floor(Math.random() * contents.length)];
+  
   return (
     <p className='text-muted-foreground max-w-[42rem] px-4 leading-normal sm:px-8 sm:text-xl sm:leading-8'>
-      Use logical reasoning and evidence to make rational decisions.
+      {randomContent}
     </p>
   );
 }
+const contents = [
+  "Think Better, Lead Better.",
+  "Sharper Thinking for Smarter Decisions.",
+  "Empowering Minds, Solving Challenges.",
+  "Master the Art of Problem-Solving.",
+  "Logic: Where Leaders Think Smarter.",
+  "Elevate Your Thinking, Transform Your Leadership.",
+  "From Insight to Impact.",
+  "Your Partner in Strategic Thinking.",
+  "Think Like a Pro, Lead Like a Pro.",
+  "Decisions Backed by Logic.",
+]
